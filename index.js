48d3a9d27de14696a18bf35da385b227a64d438f
Keelyy <ikeelyy@gmail.com>
2022-10-14 22:34:40 +0700

    bt js bc 39

---
+++ index.js
@@ -0,0 +1,166 @@
+// console.log("hello cybersoft!");
+// console.log("chelsea vô địch");
+// var username =( "chelsea v0 d1ch")
+// console.log("chelseax")
+// variable khai bao "BIEN"
+// Dâta type (kiểu dữ liệu)
+//  - string ("" nháy kép)
+//  - number (số)
+// Boolean(true/false)
+// undefined (không xác định)
+// null (có xác định)
+
+
+// var username = "helo cyber";
+// console.log("username:"  + username );
+
+// var isLogin= true
+
+// var firstName;
+// firstName = " man đần ";
+// console.log(firstName);
+// các loại toán tử 
+// plus +
+// var num1 = 100; number
+// var num2 = "21312"; string
+// var num12 = num1 + num2; number + string
+// console.log("tong" + num12); 
+
+ 
+// minus "-"
+// var num3=10;
+// var num4=5;
+// var num34 = num3 - num4
+// console.log("hieu:" + num34);
+
+
+// var num5 = 123;
+// var num6 = 12321321421;
+// var num56 = num5 * num6;
+// console.log("Tich;" + num56)
+
+
+// var num7 =123;
+// var num8 =456;
+// var num78 = num7 / num8;
+// console.log("thuong:" + num78);
+
+
+// var num9=10;
+// var num10=2;
+// var num910= num9 % num10;
+// console.log(num910);
+
+// tăng 1 đơn vị
+// var count = 0123213;
+//  count =count + 172731232132121;
+// console.log(count);
+
+
+// var newcount = count++;
+// console.log("newcount:" + newcount)
+
+
+
+// ++ : tăng 1 đơn vị
+// -- : giảm 1 đơn vị 
+// var sum = 0;
+// sum -= 5;
+// console.log(sum);
+
+// var a = 3;
+// var b = 2;
+// a += b;
+// b = ++b + ++a;
+// console.log(a,b);
+
+// hằng số (ES6)
+// const
+
+// const PI=3.14;
+// console.log(PI);
+
+
+// mô hình ba khối
+// đầu vào
+// số thứ 1 anh xoa het cung duoc 
+// số  thứ 2 
+
+
+// xử lý
+// số thứ 1 + số thứ 2
+
+
+
+
+// đầu ra 
+// in va
+
+
+
+
+
+// var n = 145;
+// var unit;
+// var ten ;
+// var hundred;
+// var sum;
+
+
+// var hundred = Math.floor(n / 100);
+// console.log(hundred);
+// var ten = Math.floor(n  % 100 / 10);
+// console.log(ten);
+// var unit = n % 10;
+// console.log(unit);
+// var sum = hundred + ten + unit;
+// console.log(sum);
+
+
+
+
+var luong1=100000;
+var songaylam;
+
+var tienluong=luong1 * songaylam;
+console.log(tienluong)
+
+
+
+var num1 = 5;
+var num2 = 10;
+var num3 = 15;
+var num4 = 20;
+var num5 = 25;
+
+var averagevalue= (num1 + num2 + num3 + num4 + num5) / 5 ;
+console.log("averagevalue =" + averagevalue);
+
+
+var vnd=23500;
+var usd ;
+var usd= vnd * usd ;
+
+console.log(usd);
+ 
+
+
+
+var dai=10;
+var rong=5;
+
+var dientich=dai*rong;
+console.log("dien tich hinh chu nhat =" + dientich);
+var chuvi=(dai + rong) * 2;
+console.log("chu vi hinh chu nhat =" + chuvi);
+
+
+var num = 12;
+
+
+var donvi= num % 10;
+var chuc = num / 10;    
+var tong = donvi + chuc; 
+console.log("tong =" + tong); 
+
+console.log(chuc);
